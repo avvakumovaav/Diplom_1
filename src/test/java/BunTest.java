98724ea3886590d2ffd7e0f51f12f049bb2be742
avvakumovaav <nikulina.anastasiya.v@yandex.ru>
2024-11-01 13:25:00 +0300

    feature: unit-test for Bun

--- src/test/java/BunTest.java
+++ src/test/java/BunTest.java
@@ -1,2 +1,48 @@
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.mockito.MockitoAnnotations;
+import praktikum.Bun;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
 public class BunTest {
+    private Bun bun;
+    private final String name;
+    private final float price;
+
+    public BunTest(String name, float price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    @Before
+    public void setUp() {
+        bun = new Bun(name, price);
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] getBunData() {
+        return new Object[][]{
+                { "Bun1", 12.3f},
+                { "Bun2", 45.0f},
+                { "Bun3", 46f},
+                { "Bun4", -0.5f},
+                { "Bun5", 0f},
+        };
+    }
+
+    @Test
+    public void getNameTest() {
+        assertEquals("Название булочки не соответствует ожидаемому", name, bun.getName());
+
+    }
+
+    @Test
+    public void getPriceTest() {
+        var delta = 0.1f;
+        assertEquals("Цена булочки не соответствует ожидаемому", price, bun.getPrice(), delta);
+    }
 }
